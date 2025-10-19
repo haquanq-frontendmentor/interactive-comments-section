c9cc0ab054ec9c3f4a7e13bebf964524d4930192
Ha Quanq <quangha0120010@gmail.com>
2025-10-19 23:35:31 +0700

    Add static images with index exports

---
+++ src/assets/images/index.ts
@@ -0,0 +1,5 @@
+export { default as DeleteIcon } from "./icon-delete.svg";
+export { default as EditIcon } from "./icon-edit.svg";
+export { default as MinusIcon } from "./icon-minus.svg";
+export { default as PlusIcon } from "./icon-plus.svg";
+export { default as ReplyIcon } from "./icon-reply.svg";
