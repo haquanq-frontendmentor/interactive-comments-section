4f839fd8a0b4cad3ee9b8bdd96226883943e180c
Ha Quanq <quangha0120010@gmail.com>
2025-10-20 21:19:40 +0700

    Update images exports as components

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,5 @@
-export { default as DeleteIcon } from "./icon-delete.svg";
-export { default as EditIcon } from "./icon-edit.svg";
-export { default as MinusIcon } from "./icon-minus.svg";
-export { default as PlusIcon } from "./icon-plus.svg";
-export { default as ReplyIcon } from "./icon-reply.svg";
+export { default as DeleteIcon } from "./icon-delete.svg?component";
+export { default as EditIcon } from "./icon-edit.svg?component";
+export { default as MinusIcon } from "./icon-minus.svg?component";
+export { default as PlusIcon } from "./icon-plus.svg?component";
+export { default as ReplyIcon } from "./icon-reply.svg?component";
