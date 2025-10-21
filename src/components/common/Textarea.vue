1a18ce1e86a374cadd497429b9aba963ca603106
Ha Quanq <quangha0120010@gmail.com>
2025-10-21 09:56:28 +0700

    Prevent Textarea resize

--- src/components/common/Textarea.vue
+++ src/components/common/Textarea.vue
@@ -4,7 +4,7 @@ import { cn } from "../../utils/cn";
 
 const props = defineProps<{ class?: string }>();
 const baseClass =
-  "h-24 rounded-lg focus:outline-2 focus:outline-offset-2 focus:outline-purple-600 px-6 py-3 w-full leading-6 inset-ring inset-ring-gray-200 transition-[box-shadow] placeholder:text-gray-500 hover:inset-ring-purple-600 ";
+  "h-24 rounded-lg focus:outline-2 resize-none focus:outline-offset-2 focus:outline-purple-600 px-6 py-3 w-full leading-6 inset-ring inset-ring-gray-200 transition-[box-shadow] placeholder:text-gray-500 hover:inset-ring-purple-600 ";
 const mergedClass = computed(() => cn(baseClass, props.class));
 
 const model = defineModel<string>();
